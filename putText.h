fdefcb34e7354fd22d4866628af46137d1ded5ed
Wang Gao <80475837@qq.com>
2018-12-31 11:31:03 +0800

    Update putText.h

--- putText.h
+++ putText.h
@@ -1,6 +1,13 @@
 #ifndef PUTTEXT_H_
 #define PUTTEXT_H_
 
+/*
+  作者：codeaholic 
+  来源：CSDN 
+  原文：https://blog.csdn.net/wanggao_1990/article/details/52955056 
+  版权声明：本文为博主原创文章，转载请附上博文链接！
+*/
+
 #include <windows.h>
 #include <string>
 #include <opencv2/opencv.hpp>
